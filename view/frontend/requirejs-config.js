eecf946e068078b210cf28c9bd251ac8cac0441b
Damien Couchez <damien.couchez@gmail.com>
2021-04-07 11:15:17 +0200

    Removing perso / insights

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,7 +1,6 @@
 var config = {
 	paths: {
 		'algoliaBundle': 'Algolia_AlgoliaSearch/internals/algoliaBundle.min',
-		'algoliaAnalytics': 'Algolia_AlgoliaSearch/internals/search-insights',
 		'rangeSlider': 'Algolia_AlgoliaSearch/navigation/range-slider-widget'
 	}
 };
