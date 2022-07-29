40b199a47cfbf2c7bf691c82dde8d9f739848221
Ratikant Singh <ratikant.singh@algolia.com>
2022-07-29 10:57:54 +0530

    MAGE-326 fix

--- view/frontend/web/internals/common.js
+++ view/frontend/web/internals/common.js
@@ -229,8 +229,8 @@ requirejs(['algoliaBundle'], function(algoliaBundle) {
                     if (color.matchLevel === undefined || color.matchLevel === 'none') {
                         return;
                     }
-
-                    colors.push(color.value);
+                    //colors.push(color.value);
+                    colors.push(color);
 
                     if (algoliaConfig.useAdaptiveImage === true) {
                         var matchedColor = color.matchedWords.join(' ');
@@ -240,9 +240,9 @@ requirejs(['algoliaBundle'], function(algoliaBundle) {
                     }
                 });
 
-                colors = colors.join(', ');
-
-                hit._highlightResult.color = { value: colors };
+                //colors = colors.join(', ');
+                //hit._highlightResult.color = { value: colors };
+                hit._highlightResult.color = colors;
             }
             else {
                 if (hit._highlightResult.color && hit._highlightResult.color.matchLevel === 'none') {
