70b34222d0d39bb4691ef4f776410dd2c4472a6d
Mohit Choudhary <mohit.choudhary@algolia.com>
2022-09-09 16:08:00 +0530

    Added addtocart button in recommends item and refactor recommend js

--- view/frontend/web/recommend.js
+++ view/frontend/web/recommend.js
@@ -2,8 +2,9 @@ requirejs([
     'jquery',
     'recommend',
     'recommendJs',
-    'algoliaBundle'
-], function ($, recommend, recommendJs, algoliaBundle) {
+    'algoliaBundle',
+    'mage/translate'
+], function ($, recommend, recommendJs, algoliaBundle, $tr) {
     this.config = algoliaConfig;
     this.defaultIndexName = algoliaConfig.indexName + '_products';
     const appId = this.config.applicationId;
@@ -21,25 +22,11 @@ requirejs([
                     objectIDs: [currentObjectID],
                     maxRecommendations: this.config.recommend.limitFBTProducts,
                     itemComponent({item, createElement, Fragment}) {
-                        var correctFKey = getCookie('form_key');
-                        var action = config.instant.addToCartParams.action + 'product/' + item.objectID + '/';
-                        if(correctFKey != "" && config.instant.addToCartParams.formKey != correctFKey) {
-                            config.instant.addToCartParams.formKey = correctFKey;
+                        if (config.recommend.isAddToCartEnabledInFBT) {
+                            return renderRecommendDataWithAddToCart(item, createElement);
+                        }else{
+                            return renderRecommendData(item, createElement);
                         }
-                        return createElement(
-                            'div',
-                            null,
-                            createElement(
-                                'div',
-                                {className: "product-details"},
-                                createElement(
-                                    'a',
-                                    {className: "product-url", href: item.url},
-                                    createElement('img', {className: "product-img", src: item.image_url}, item.image_url),
-                                    createElement('p', {className: "product-name"}, item.name)
-                                )
-                            )
-                        );
                     },
                 });
             }
@@ -51,105 +38,91 @@ requirejs([
                     objectIDs: [currentObjectID],
                     maxRecommendations: this.config.recommend.limitRelatedProducts,
                     itemComponent({item, createElement, Fragment}) {
-                        var correctFKey = getCookie('form_key');
-                        var action = config.instant.addToCartParams.action + 'product/' + item.objectID + '/';
-                        if(correctFKey != "" && config.instant.addToCartParams.formKey != correctFKey) {
-                            config.instant.addToCartParams.formKey = correctFKey;
+                        if (config.recommend.isAddToCartEnabledInRelatedProduct) {
+                            return renderRecommendDataWithAddToCart(item, createElement);;
+                        }else{
+                            return renderRecommendData(item, createElement)
                         }
-                        return createElement(
-                            'div',
-                            null,
-                            createElement(
-                                'div',
-                                {className: "product-details"},
-                                createElement(
-                                    'a',
-                                    {className: "product-url", href: item.url},
-                                    createElement('img', {className: "product-img", src: item.image_url}, item.image_url),
-                                    createElement('p', {className: "product-name"}, item.name)
-                                )
-                            )
-                        );
                     },
                 });
             }
         }
 
     if (this.config.recommend.enabledTrendItems && ((this.config.recommend.isTrendItemsEnabledInPDP && $('body').hasClass('catalog-product-view')) || (this.config.recommend.isTrendItemsEnabledInCartPage && $('body').hasClass('checkout-cart-index'))) ) {
-        if(this.config.recommend.trendItemsType == "trending_items_for_facets" && this.config.recommend.trendItemFacetName && this.config.recommend.trendItemFacetValue){
-            recommendJs.trendingItems({
-                container: '#trendItems',
-                facetName: this.config.recommend.trendItemFacetName,
-                facetValue: this.config.recommend.trendItemFacetValue,
-                recommendClient,
-                indexName,
-                maxRecommendations: this.config.recommend.limitTrendingItems,
-                itemComponent({item, createElement, Fragment}) {
-                    return createElement(
-                        'div',
-                        null,
-                        createElement(
-                            'div',
-                            {className: "product-details"},
-                            createElement(
-                                'a',
-                                {className: "product-url", href: item.url},
-                                createElement('img', {className: "product-img", src: item.image_url}, item.image_url),
-                                createElement('p', {className: "product-name"}, item.name)
-                            )
-                        )
-                    );
-                },
-            });
-        } else {
-            recommendJs.trendingItems({
-                container: '#trendItems',
-                recommendClient,
-                indexName,
-                maxRecommendations: this.config.recommend.limitTrendingItems,
-                itemComponent({item, createElement, Fragment}) {
-                    return createElement(
-                        'div',
-                        null,
-                        createElement(
-                            'div',
-                            {className: "product-details"},
-                            createElement(
-                                'a',
-                                {className: "product-url", href: item.url},
-                                createElement('img', {className: "product-img", src: item.image_url}, item.image_url),
-                                createElement('p', {className: "product-name"}, item.name)
-                            )
-                        )
-                    );
-                },
-            });
-        }
-    }else if(this.config.recommend.enabledTrendItems && typeof recommendTrendContainer !== "undefined" && typeof facetName !== "undefined" && typeof facetValue !== "undefined"){
-        let containerValue = "#"+recommendTrendContainer;
         recommendJs.trendingItems({
-            container: containerValue,
-            facetName: facetName,
-            facetValue: facetValue,
+            container: '#trendItems',
+            facetName: this.config.recommend.trendItemFacetName ? this.config.recommend.trendItemFacetName : '',
+            facetValue: this.config.recommend.trendItemFacetValue ? this.config.recommend.trendItemFacetValue : '',
             recommendClient,
             indexName,
             maxRecommendations: this.config.recommend.limitTrendingItems,
             itemComponent({item, createElement, Fragment}) {
-                return createElement(
-                    'div',
-                    null,
-                    createElement(
-                        'div',
-                        {className: "product-details"},
-                        createElement(
-                            'a',
-                            {className: "product-url", href: item.url},
-                            createElement('img', {className: "product-img", src: item.image_url}, item.image_url),
-                            createElement('p', {className: "product-name"}, item.name)
-                        )
-                    )
-                );
+                if (config.recommend.isAddToCartEnabledInTrendsItem) {
+                    return renderRecommendDataWithAddToCart(item, createElement);;
+                }else{
+                    return renderRecommendData(item, createElement)
+                }
             },
         });
+    }else if(this.config.recommend.enabledTrendItems && typeof recommendTrendContainer !== "undefined"){
+        let containerValue = "#"+recommendTrendContainer;
+        recommendJs.trendingItems({
+            container: containerValue,
+            facetName: facetName ? facetName : '',
+            facetValue: facetValue ? facetValue : '',
+            recommendClient,
+            indexName,
+            maxRecommendations: this.config.recommend.limitTrendingItems,
+            itemComponent({item, createElement, Fragment}) {
+                if (config.recommend.isAddToCartEnabledInTrendsItem) {
+                    return renderRecommendDataWithAddToCart(item, createElement);;
+                }else{
+                    return renderRecommendData(item, createElement)
+                }
+            },
+        });
+    }
+    function renderRecommendData(item, createElement){
+        return createElement(
+            'div',
+            null,
+            createElement(
+                'div',
+                {className: "product-details"},
+                createElement(
+                    'a',
+                    {className: "product-url", href: item.url},
+                    createElement('img', {className: "product-img", src: item.image_url}, item.image_url),
+                    createElement('p', {className: "product-name"}, item.name)
+                )
+            )
+        );
+    }
+    function renderRecommendDataWithAddToCart(item, createElement){
+        let correctFKey = getCookie('form_key');
+        let action = config.recommend.addToCartParams.action + 'product/' + item.objectID + '/';
+        if(correctFKey != "" && config.recommend.addToCartParams.formKey != correctFKey) {
+            config.recommend.addToCartParams.formKey = correctFKey;
+        }
+        return createElement(
+            'div',
+            null,
+            createElement(
+                'div',
+                {className: "product-details"},
+                createElement(
+                    'a',
+                    {className: "product-url", href: item.url},
+                    createElement('img', {className: "product-img", src: item.image_url}, item.image_url),
+                    createElement('p', {className: "product-name"}, item.name),
+                    createElement('form', {className: 'addTocartForm', action: action, method: 'post'},
+                        createElement('input', {type: 'hidden', name: 'form_key',value: config.recommend.addToCartParams.formKey}),
+                        createElement('input', {type: 'hidden', name:'unec', value: AlgoliaBase64.mageEncode(action)}),
+                        createElement('input', {type: 'hidden', name:'product', value: item.objectID}),
+                        createElement('button', {type: 'submit', className: 'action tocart primary'}, $tr('Add To Cart'))
+                    )
+                )
+            )
+        );
     }
 });
