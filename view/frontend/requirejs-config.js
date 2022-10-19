1ed70094adc7da10816b42b197159724d4cf81c4
Ratikant Singh <ratikant.singh@algolia.com>
2022-10-19 13:14:52 +0530

    MAGE-448

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -5,11 +5,11 @@ var config = {
         'recommend': 'Algolia_AlgoliaSearch/internals/recommend.min',
         'recommendJs': 'Algolia_AlgoliaSearch/internals/recommend-js.min',
 		'rangeSlider': 'Algolia_AlgoliaSearch/navigation/range-slider-widget',
-        'pagesHtml': 'Algolia_AlgoliaSearch/internals/template/pages',
-        'categoriesHtml': 'Algolia_AlgoliaSearch/internals/template/categories',
-        'productsHtml': 'Algolia_AlgoliaSearch/internals/template/products',
-        'suggestionsHtml': 'Algolia_AlgoliaSearch/internals/template/suggestions',
-        'additionalHtml': 'Algolia_AlgoliaSearch/internals/template/additional-section'
+        'pagesHtml': 'Algolia_AlgoliaSearch/internals/template/autocomplete/pages',
+        'categoriesHtml': 'Algolia_AlgoliaSearch/internals/template/autocomplete/categories',
+        'productsHtml': 'Algolia_AlgoliaSearch/internals/template/autocomplete/products',
+        'suggestionsHtml': 'Algolia_AlgoliaSearch/internals/template/autocomplete/suggestions',
+        'additionalHtml': 'Algolia_AlgoliaSearch/internals/template/autocomplete/additional-section'
 	},
 	config: {
 		mixins: {
