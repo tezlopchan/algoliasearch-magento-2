6068d6b26f2d09391ab61bf2fe38b8aff2b667ae
Mohit Choudhary <mohit.choudhary@algolia.com>
2022-11-28 17:22:01 +0530

    Refactored the Algolia Recommend code

--- view/frontend/web/recommend.js
+++ view/frontend/web/recommend.js
@@ -1,86 +1,89 @@
-requirejs([
+define([
     'algoliaBundle',
     'recommend',
     'recommendJs',
     'recommendItemsElement',
     'domReady!'
-], function (algoliaBundle, recommend, recommendJs, recommendItemsElement) {
-    algoliaBundle.$(function ($) {
-        this.defaultIndexName = algoliaConfig.indexName + '_products';
-        const appId = algoliaConfig.applicationId;
-        const apiKey = algoliaConfig.apiKey;
-        const recommendClient = recommend(appId, apiKey);
-        const indexName = this.defaultIndexName;
-        if ($('body').hasClass('catalog-product-view') || $('body').hasClass('checkout-cart-index')) {
-            // --- Add the current product objectID here ---
-            if ((algoliaConfig.recommend.enabledFBT && $('body').hasClass('catalog-product-view')) || (algoliaConfig.recommend.enabledFBTInCart && $('body').hasClass('checkout-cart-index'))) {
-                recommendJs.frequentlyBoughtTogether({
-                    container: '#frequentlyBoughtTogether',
-                    recommendClient,
-                    indexName,
-                    objectIDs: algoliObjectId,
-                    maxRecommendations: algoliaConfig.recommend.limitFBTProducts,
-                    itemComponent({item, createElement, Fragment}) {
-                        console.log(algoliaConfig);
-                        if (algoliaConfig.recommend.isAddToCartEnabledInFBT) {
-                            return recommendItemsElement.getRecommendItemsWithAddToCart(item, createElement);
-                        } else {
-                            return recommendItemsElement.getRecommendDataWithNoAddToCart(item, createElement);
-                        }
-                    },
-                });
-            }
-            if ((algoliaConfig.recommend.enabledRelated && $('body').hasClass('catalog-product-view')) || (algoliaConfig.recommend.enabledRelatedInCart && $('body').hasClass('checkout-cart-index'))) {
-                recommendJs.relatedProducts({
-                    container: '#relatedProducts',
-                    recommendClient,
-                    indexName,
-                    objectIDs: algoliObjectId,
-                    maxRecommendations: algoliaConfig.recommend.limitRelatedProducts,
-                    itemComponent({item, createElement, Fragment}) {
-                        if (algoliaConfig.recommend.isAddToCartEnabledInRelatedProduct) {
-                            return recommendItemsElement.getRecommendItemsWithAddToCart(item, createElement);
-                        } else {
-                            return recommendItemsElement.getRecommendDataWithNoAddToCart(item, createElement);
-                        }
-                    },
-                });
-            }
-        }
+],function (algoliaBundle, recommend, recommendJs, recommendItemsElement) {
+    'use strict';
 
-        if ((algoliaConfig.recommend.isTrendItemsEnabledInPDP && $('body').hasClass('catalog-product-view')) || (algoliaConfig.recommend.isTrendItemsEnabledInCartPage && $('body').hasClass('checkout-cart-index'))) {
-            recommendJs.trendingItems({
-                container: '#trendItems',
-                facetName: algoliaConfig.recommend.trendItemFacetName ? algoliaConfig.recommend.trendItemFacetName : '',
-                facetValue: algoliaConfig.recommend.trendItemFacetValue ? algoliaConfig.recommend.trendItemFacetValue : '',
-                recommendClient,
-                indexName,
-                maxRecommendations: algoliaConfig.recommend.limitTrendingItems,
-                itemComponent({item, createElement, Fragment}) {
-                    if (algoliaConfig.recommend.isAddToCartEnabledInTrendsItem) {
-                        return recommendItemsElement.getRecommendItemsWithAddToCart(item, createElement);
-                    } else {
-                        return recommendItemsElement.getRecommendDataWithNoAddToCart(item, createElement);
-                    }
-                },
-            });
-        } else if (algoliaConfig.recommend.enabledTrendItems && typeof recommendTrendContainer !== "undefined") {
-            let containerValue = "#" + recommendTrendContainer;
-            recommendJs.trendingItems({
-                container: containerValue,
-                facetName: facetName ? facetName : '',
-                facetValue: facetValue ? facetValue : '',
-                recommendClient,
-                indexName,
-                maxRecommendations: numOfTrendsItem ? parseInt(numOfTrendsItem) : algoliaConfig.recommend.limitTrendingItems,
-                itemComponent({item, createElement, Fragment}) {
-                    if (algoliaConfig.recommend.isAddToCartEnabledInTrendsItem) {
-                        return recommendItemsElement.getRecommendItemsWithAddToCart(item, createElement);
-                    } else {
-                        return recommendItemsElement.getRecommendDataWithNoAddToCart(item, createElement);
-                    }
-                },
-            });
-        }
-    });
+    return function (config, element) {
+        algoliaBundle.$(function ($) {
+            this.defaultIndexName = algoliaConfig.indexName + '_products';
+            const appId = algoliaConfig.applicationId;
+            const apiKey = algoliaConfig.apiKey;
+            const recommendClient = recommend(appId, apiKey);
+            const indexName = this.defaultIndexName;
+            if ($('body').hasClass('catalog-product-view') || $('body').hasClass('checkout-cart-index')) {
+                // --- Add the current product objectID here ---
+                if ((algoliaConfig.recommend.enabledFBT && $('body').hasClass('catalog-product-view')) || (algoliaConfig.recommend.enabledFBTInCart && $('body').hasClass('checkout-cart-index'))) {
+                    recommendJs.frequentlyBoughtTogether({
+                        container: '#frequentlyBoughtTogether',
+                        recommendClient,
+                        indexName,
+                        objectIDs: config.algoliObjectId,
+                        maxRecommendations: algoliaConfig.recommend.limitFBTProducts,
+                        itemComponent({item, createElement, Fragment}) {
+                            if (algoliaConfig.recommend.isAddToCartEnabledInFBT) {
+                                return recommendItemsElement.getRecommendItemsWithAddToCart(item, createElement);
+                            } else {
+                                return recommendItemsElement.getRecommendDataWithNoAddToCart(item, createElement);
+                            }
+                        },
+                    });
+                }
+                if ((algoliaConfig.recommend.enabledRelated && $('body').hasClass('catalog-product-view')) || (algoliaConfig.recommend.enabledRelatedInCart && $('body').hasClass('checkout-cart-index'))) {
+                    recommendJs.relatedProducts({
+                        container: '#relatedProducts',
+                        recommendClient,
+                        indexName,
+                        objectIDs: config.algoliObjectId,
+                        maxRecommendations: algoliaConfig.recommend.limitRelatedProducts,
+                        itemComponent({item, createElement, Fragment}) {
+                            if (algoliaConfig.recommend.isAddToCartEnabledInRelatedProduct) {
+                                return recommendItemsElement.getRecommendItemsWithAddToCart(item, createElement);
+                            } else {
+                                return recommendItemsElement.getRecommendDataWithNoAddToCart(item, createElement);
+                            }
+                        },
+                    });
+                }
+            }
+
+            if ((algoliaConfig.recommend.isTrendItemsEnabledInPDP && $('body').hasClass('catalog-product-view')) || (algoliaConfig.recommend.isTrendItemsEnabledInCartPage && $('body').hasClass('checkout-cart-index'))) {
+                recommendJs.trendingItems({
+                    container: '#trendItems',
+                    facetName: algoliaConfig.recommend.trendItemFacetName ? algoliaConfig.recommend.trendItemFacetName : '',
+                    facetValue: algoliaConfig.recommend.trendItemFacetValue ? algoliaConfig.recommend.trendItemFacetValue : '',
+                    recommendClient,
+                    indexName,
+                    maxRecommendations: algoliaConfig.recommend.limitTrendingItems,
+                    itemComponent({item, createElement, Fragment}) {
+                        if (algoliaConfig.recommend.isAddToCartEnabledInTrendsItem) {
+                            return recommendItemsElement.getRecommendItemsWithAddToCart(item, createElement);
+                        } else {
+                            return recommendItemsElement.getRecommendDataWithNoAddToCart(item, createElement);
+                        }
+                    },
+                });
+            } else if (algoliaConfig.recommend.enabledTrendItems && typeof config.recommendTrendContainer !== "undefined") {
+                let containerValue = "#" + config.recommendTrendContainer;
+                recommendJs.trendingItems({
+                    container: containerValue,
+                    facetName: config.facetName ? config.facetName : '',
+                    facetValue: config.facetValue ? config.facetValue : '',
+                    recommendClient,
+                    indexName,
+                    maxRecommendations: config.numOfTrendsItem ? parseInt(config.numOfTrendsItem) : algoliaConfig.recommend.limitTrendingItems,
+                    itemComponent({item, createElement, Fragment}) {
+                        if (algoliaConfig.recommend.isAddToCartEnabledInTrendsItem) {
+                            return recommendItemsElement.getRecommendItemsWithAddToCart(item, createElement);
+                        } else {
+                            return recommendItemsElement.getRecommendDataWithNoAddToCart(item, createElement);
+                        }
+                    },
+                });
+            }
+        });
+    }
 });
